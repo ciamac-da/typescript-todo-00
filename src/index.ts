77adf4c1df046fb31c04f572e2a00765f1702eb1
ciamac-da <ciamac.davoudi@gmail.com>
2022-04-30 23:43:04 +0200

    Verified by Marti4n v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
-//import {v4 as uuidV4} from "uuid";
-//console.log(uuidV4())
+import {v4 as uuidV4} from "uuid";
 
-const list = document.querySelector("#list")
-console.log(list)
+type Task = { 
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date 
+}
+
+const list = document.querySelector("#list") as HTMLUListElement | null
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+  
+  if(input?.value == "" || input?.value == null) return
+
+  const newTask: Task = { 
+    id: uuidV4(),
+    title: input.value,
+    completed: true,
+    createdAt: new Date()
+  }
+  tasks.push(newTask)
+  saveTasks()
+
+
+  addListItem(newTask)
+  input.value = ""
+})
+
+function addListItem(task:Task): boolean{
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+  return true;
+}
+
+function saveTasks() {
+  return localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[]{
+  const tasksJSON = localStorage.getItem("TASKS")
+  if (tasksJSON === null) return []
+  return JSON.parse(tasksJSON)
+}
